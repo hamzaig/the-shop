884dcfa21fa6e4936d20f6af98709405ab095af2
hamzaig <hamzaig@yahoo.com>
2022-03-11 17:37:51 +0500

    changing port issue

--- backend/server.js
+++ backend/server.js
@@ -47,4 +47,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 7000;
 
-app.listen("7000", console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`.green.bold));
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`.green.bold));
