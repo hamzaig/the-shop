4cf4bb822f1faa21b4d1f501d4ebd12d80f63dbb
hamzaig <hamzaig@yahoo.com>
2022-03-11 15:08:43 +0500

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,23 @@ app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running")
-})
 
 // console.log(path.join(__dirname, "/public"));
 
 // const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..//frontend/build")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"));
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running")
+  })
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
